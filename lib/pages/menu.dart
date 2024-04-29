77d968e32ef524bae5f9dce918bc0b9a6d62801b
unknown <e.novelo3@hotmail.com>
2024-04-29 12:16:20 -0600

    First commit

---
+++ lib/pages/menu.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Menu extends StatelessWidget {
+  const Menu({super.key, required String title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          
+        ),
+        body: CircleAvatar(),
+    );
+  }
+}
